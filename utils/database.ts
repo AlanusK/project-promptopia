874ac82d56bf7652208e181d1e1c028a68cd9bb6
AlanusK <akajara@gmail.com>
2024-08-19 18:40:06 +0300

    fix: mongoose error insuring first 'uri' parameter is a string

--- utils/database.ts
+++ utils/database.ts
@@ -11,7 +11,7 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI!, {
+    await mongoose.connect(process.env.MONGODB_URI as string, {
       dbName: "share_prompt",
     } as ConnectOptions);
 
