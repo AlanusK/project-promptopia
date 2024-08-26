9e35691469a88ca6d3956697cc8a30d237667130
AlanusK <akajara@gmail.com>
2024-08-26 22:37:58 +0300

    fix: fix data updating on a deployed app

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -1,6 +1,8 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
+export const dynamic = 'force-dynamic';
+
 export const GET = async (request: Request) => {
   try {
     await connectToDB();
