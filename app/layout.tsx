9acbba0f65eb7e8e932c5c7a9ea08f2f4a44e488
AlanusK <akajara@gmail.com>
2024-08-26 21:50:21 +0300

    fix: fix missing Suspense boundary with useSearchParams

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,29 +2,29 @@ import "@styles/globals.css";
 
 import Provider from "@components/Provider";
 import Nav from "@components/Nav";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
 };
 
-const RootLayout = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body>
         <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
+          <Suspense fallback={<p>Loading...</p>}>
+            <div className="main">
+              <div className="gradient" />
+            </div>
 
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
+            <main className="app">
+              <Nav />
+
+              {children}
+            </main>
+          </Suspense>
         </Provider>
       </body>
     </html>
